2a95d77efe6c1e0c9cddda3b6fd573edfa67b014
MFQ <softmfq@gmail.com>
2015-12-14 14:53:43 +0500

    updating UX and send message funcationalty added

--- client/scripts/controllers/chat.controller.js
+++ client/scripts/controllers/chat.controller.js
@@ -2,12 +2,20 @@ angular
   .module('Whatsapp')
   .controller('ChatCtrl', ChatCtrl);
  
-function ChatCtrl ($scope, $reactive, $stateParams) {
+function ChatCtrl ($scope, $reactive, $stateParams, $ionicScrollDelegate, $timeout) {
   $reactive(this).attach($scope);
  
   let chatId = $stateParams.chatId;
+  let isIOS = ionic.Platform.isWebView() && ionic.Platform.isIOS();
+
+  this.inputUp = inputUp
+  this.inputDown = inputDown
+  this.closeKeyboard = closeKeyboard
+
   this.sendMessage = sendMessage;
- 
+  
+  console.log($timeout)
+
   this.helpers({
   	messages() {
       return Messages.find({ chatId: chatId });
@@ -16,8 +24,37 @@ function ChatCtrl ($scope, $reactive, $stateParams) {
       return Chats.findOne(chatId);
     }
   });
-}
 
-function sendMessage(){
-	
+  function sendMessage(){
+    if (_.isEmpty(this.message)) return;
+ 
+    Meteor.call('newMessage', {
+      text: this.message,
+      type: 'text',
+      chatId: chatId
+    });
+ 
+    delete this.message;
+  }
+
+  function inputUp () {
+    if (isIOS) {
+      this.keyboardHeight = 216;
+    }
+
+    $timeout(function() {
+      $ionicScrollDelegate.$getByHandle('chatScroll').scrollBottom(true);
+    }, 300);
+  }
+
+  function inputDown(){
+    if (isIOS) {
+      this.keyboardHeight = 0;
+    }
+    $ionicScrollDelegate.$getByHandle('chatScroll').resize();
+  }
+
+  function closeKeyboard(){}
+
+
 }
