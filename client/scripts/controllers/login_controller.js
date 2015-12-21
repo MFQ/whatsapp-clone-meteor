e6b732e3cfbfcfbfa46fd7485726bf952fd12cf7
MFQ <softmfq@gmail.com>
2015-12-22 01:32:24 +0500

    login_controller is in progress

---
+++ client/scripts/controllers/login_controller.js
@@ -0,0 +1,7 @@
+angular.module("Whatsapp").controller("LoginCtrl", LoginCtrl);
+
+function LoginCtrl($scope, $reactive, $state, $ionicLoading, $ionicPopup, $log){
+	$reactive(this).attact($scope);
+
+	this.login = login
+}
